841a28ab8fd2f4c7101ff15562a792cf5387cac0
tony-cypher <tonycypher0@gmail.com>
2024-11-22 05:24:29 +0100

    Initialized the project

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/posts", postRoutes);
+// app.use("/api/notifications", notificationRoutes);
+
+app.listen(PORT, () => {
+  //connectDB();
+  console.log("Server is running on port 5000");
+});
