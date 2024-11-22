ae2839d13c13325438ff6e3d2a8d37174b04e540
tony-cypher <tonycypher0@gmail.com>
2024-11-22 11:58:20 +0100

    Added logout api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -92,3 +92,13 @@ export const login = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout contoller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
