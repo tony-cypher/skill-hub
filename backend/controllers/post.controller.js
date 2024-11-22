31199977a71922055387bbde2c88b9b87021ee9c
tony-cypher <tonycypher0@gmail.com>
2024-11-22 16:17:00 +0100

    Added post delete api

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -247,3 +247,37 @@ export const commentOnPost = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const deletePost = async (req, res) => {
+  try {
+    // gets the post to be deleted by id
+    const post = await Post.findById(req.params.id);
+
+    // checks if the post exists
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    // checks if the req.user is the post.user(owner)
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "You are not allowed to delete this post" });
+    }
+
+    // deleted the post from cloudinary, if the post exists
+    if (post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
+
+    // deletes the post from the db
+    await Post.findByIdAndDelete(req.params.id);
+
+    // sends a success message
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.log("Error in deletePost controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -7,18 +7,17 @@ import {
   workedFor,
   likeUnlikePost,
   commentOnPost,
+  deletePost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/all", protectRoute, getAllPosts);
-// router.get("/likedposts/:id", protectRoute, getLikedPosts);
-// router.get("/following", protectRoute, getFollowingPosts);
 router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
 router.post("/worked/:id", protectRoute, workedFor);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
-// router.delete("/delete/:id", protectRoute, deletePost);
+router.delete("/delete/:id", protectRoute, deletePost);
 
 export default router;
