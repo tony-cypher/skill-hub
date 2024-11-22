e778eacf5288deed2906812c4de27a2e2e1841fc
tony-cypher <tonycypher0@gmail.com>
2024-11-22 14:02:20 +0100

    Added get user posts api

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost, getAllPosts } from "../controllers/post.controller.js";
+import {
+  createPost,
+  getAllPosts,
+  getUserPosts,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/all", protectRoute, getAllPosts);
 // router.get("/likedposts/:id", protectRoute, getLikedPosts);
 // router.get("/following", protectRoute, getFollowingPosts);
-// router.get("/user/:username", protectRoute, getUserPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
 // router.post("/like/:id", protectRoute, likeUnlikePost);
 // router.post("/comment/:id", protectRoute, commentOnPost);
