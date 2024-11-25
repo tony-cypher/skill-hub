9ecf8e288a5391f188edb78099857f6cd27a5f7e
tony-cypher <tonycypher0@gmail.com>
2024-11-25 06:35:27 +0100

    Designed signup/login UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+// import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
